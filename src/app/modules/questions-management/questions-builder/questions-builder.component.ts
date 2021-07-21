10af3bf3403852e9f41bd7f461b4273457964318
werstak <Webproger1@gmail.com>
2021-07-21 13:36:29 +0300

    adding answers to a question via FormArray

--- src/app/modules/questions-management/questions-builder/questions-builder.component.ts
+++ src/app/modules/questions-management/questions-builder/questions-builder.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { QuestionsInterface } from '../../../shared/interfaces/questions.interface';
-import { AnswersInterface } from '../../../shared/interfaces/answers.interface';
 
 @Component({
   selector: 'app-questions-builder',
@@ -11,8 +10,6 @@ import { AnswersInterface } from '../../../shared/interfaces/answers.interface';
 export class QuestionsBuilderComponent implements OnInit {
   questionTitle: QuestionsInterface;
   formQuestionsBuilder: FormGroup;
-  answer: AnswersInterface;
-
 
   selectedType: {}[] = [
     {questionType: 'radio', value: 'Single choice - the ability to choose only one option from the proposed'},
@@ -21,7 +18,7 @@ export class QuestionsBuilderComponent implements OnInit {
   ];
 
   constructor(
-    private fb: FormBuilder,
+    private fb: FormBuilder
   ) {
   }
 
@@ -36,8 +33,9 @@ export class QuestionsBuilderComponent implements OnInit {
         Validators.minLength(3),
         Validators.maxLength(250)])
       ],
-      questionType: ['', Validators.required],
-      answers: this.fb.group({
+      questionType: [null, Validators.required],
+
+      options: this.fb.array([this.fb.group({
         value: [null, Validators.compose([
           Validators.required,
           Validators.minLength(3),
@@ -48,11 +46,47 @@ export class QuestionsBuilderComponent implements OnInit {
           Validators.minLength(3),
           Validators.maxLength(200)])
         ],
-      }),
+      })]),
+
+      // options: this.fb.group({
+      //   value: [null, Validators.compose([
+      //     Validators.required,
+      //     Validators.minLength(3),
+      //     Validators.maxLength(10)])
+      //   ],
+      //   title: [null, Validators.compose([
+      //     Validators.required,
+      //     Validators.minLength(3),
+      //     Validators.maxLength(200)])
+      //   ],
+      // }),
+
     });
+
   }
 
-  submit(): void {
+  get options(): Array<AbstractControl> {
+    return (this.formQuestionsBuilder.get('options') as FormArray).controls;
+  }
+
+  addNewAnswer(): void {
+    const answersModel = this.fb.group({
+      value: [null, Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(10)])
+      ],
+      title: [null, Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(200)])
+      ],
+    });
+    (this.formQuestionsBuilder.get('options') as FormArray).push(answersModel);
+
+  }
+
+  submitQuestion(): void {
     console.log('formQuestionsBuilder', this.formQuestionsBuilder.value);
     this.formQuestionsBuilder.reset();
   }
